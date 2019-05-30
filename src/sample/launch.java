314350d925fae302d56aea035fdfb05ab2670869
unknown <sivapriya3793@gmail.com>
2019-05-30 10:59:16 +0530

    changes by user1

--- src/sample/launch.java
+++ src/sample/launch.java
@@ -6,14 +6,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class launch {
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sivapriya\\eclipse-workspace\\automation\\drivers\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();	
 		driver.get("https://www.facebook.com");
 		WebElement txtuser = driver.findElement(By.id("email"));
-		txtuser.sendKeys("sivapriya3793@gmail.com");
+		txtuser.sendKeys("priyayadav@gmail.com");
 		WebElement textpass = driver.findElement(By.name("pass"));
-		textpass.sendKeys("SpB@3793");
+		textpass.sendKeys("123");
 		WebElement btnlogin=driver.findElement(By.xpath("//input[@Value='Log In']"));
 		btnlogin.click();
 		
--- src/sample/webDriver.java
+++ src/sample/webDriver.java
@@ -2,14 +2,14 @@ package sample;
 
 public class webDriver {
 
-	public void get(String string) {
-		// TODO Auto-generated method stub
+	public void  get(int string) {
+		
+	
+		
 		
 	}
 
-	public void close() {
-		// TODO Auto-generated method stub
-		
-	}
 
+		
+		
 }
